308a9aa26014189e76e6bc98aef86fabf5c5f293
okekefrancis112 <okeke98@gmail.com>
2025-04-28 10:36:37 +0100

    feat: added movie modal

--- components/accountMenu.tsx
+++ components/accountMenu.tsx
@@ -1,11 +1,13 @@
 import { signOut } from "next-auth/react";
 import React from "react";
+import useCurrentUser from "../hooks/useCurrentUser";
 
 interface AccountMenuProps {
   visible?: boolean;
 }
 
 const AccountMenu: ( React.FC<AccountMenuProps>) = ({ visible }) => {
+  const  { data } = useCurrentUser();
     if (!visible) return null;
 
   return (
@@ -14,7 +16,7 @@ const AccountMenu: ( React.FC<AccountMenuProps>) = ({ visible }) => {
             <div className="px-3 group/item flex flex-row gap-3 w-full items-center">
                 <img className="w-8 rounded-md" src="/images/default-blue.png" alt=""/>
                 <p className="text-white text-sm group-hover/item:underline">
-                    Username
+                    {data?.name}
                 </p>
             </div>
             <hr className="bg-gray-600 border-0 h-px my-4" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Billboard from "../components/billBoard";
 import MovieList from "../components/movieList";
 import useMovieList from "../hooks/useMovieList";
 import useFavorites from "../hooks/useFavorites";
+import InfoModal from "../components/infoModal";
+import useInfoModal from "../hooks/useInfoModal";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -25,9 +27,11 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home() {
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
     <>
+    <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
